db830b6822947ca0dff451ea92c69bbd669ede0b
AbdullahGalalMohamed <36282511+AbdullahGalalMohamed@users.noreply.github.com>
2018-02-20 22:23:10 +0200

    Add files via upload
    Traffic lights using arduino

---
+++ Traffic_signals.ino
@@ -0,0 +1,32 @@
+int red = 13 ;
+int yellow = 7 ;
+int green = 2 ;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow , OUTPUT );
+  pinMode(green , OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red , HIGH) ;
+  digitalWrite(yellow , LOW ) ;
+  digitalWrite( green , LOW ) ;
+  delay(2000);
+  
+  digitalWrite(red , LOW) ;
+  digitalWrite(yellow , HIGH ) ;
+  digitalWrite( green , LOW ) ;
+  delay(1000);
+  
+  digitalWrite(red , LOW) ;
+  digitalWrite(yellow , LOW ) ;
+  digitalWrite( green , HIGH ) ;
+  delay(2000);
+
+  digitalWrite(red , LOW) ;
+  digitalWrite(yellow , HIGH ) ;
+  digitalWrite( green , LOW ) ;
+  delay(1000);
+  
+}
