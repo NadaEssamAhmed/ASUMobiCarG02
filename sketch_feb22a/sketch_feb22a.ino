05a0b832622a7ef7176ef5de8c6ad1e64e7636be
NadaEssamAhmed <36282357+NadaEssamAhmed@users.noreply.github.com>
2018-02-23 01:04:09 +0200

    testing code for 4 motors

---
+++ sketch_feb22a/sketch_feb22a.ino
@@ -0,0 +1,167 @@
+#include <AFMotor.h>      //add Adafruit Motor Shield library
+
+#define MAX_SPEED 160 // sets speed of DC traction motors to 150/250 or about 70% of full speed - to get power drain down.
+#define MAX_SPEED_OFFSET 40 // this sets offset to allow for differences between the two DC traction motors
+
+AF_DCMotor leftMotor1(1, MOTOR12_1KHZ); // create motor #1 using M1 output on Motor Drive Shield, set to 1kHz PWM frequency
+AF_DCMotor leftMotor2(2, MOTOR12_1KHZ); // create motor #2, using M2 output, set to 1kHz PWM frequency
+AF_DCMotor rightMotor1(3, MOTOR34_1KHZ);// create motor #3, using M3 output, set to 1kHz PWM frequency
+AF_DCMotor rightMotor2(4, MOTOR34_1KHZ);// create motor #4, using M4 output, set to 1kHz PWM frequency
+String motorSet = "";
+int speedSet = 0;
+const int BTState = A5;
+int state;
+char Blutoothe;
+
+void setup() {
+  Serial.begin(9600); //start serial communication
+  
+}
+void loop() {
+  if(Blutoothe=="M"){
+if(digitalRead(BTState)==LOW) { state='S'; }
+  while (Serial.available()){ //Check if there is an available byte to read
+delay(10); //Delay added to make thing stable
+char c = Serial.read(); //Conduct a serial read
+  }
+  }}
+  void moveStop() {leftMotor1.run(RELEASE); leftMotor2.run(RELEASE); rightMotor1.run(RELEASE); rightMotor2.run(RELEASE);}  // stop the motors.
+//-------------------------------------------------------------------------------------------------------------------------------------
+void moveForward() {
+    motorSet = "FORWARD";
+    leftMotor1.run(FORWARD);      // turn it on going forward
+    leftMotor2.run(FORWARD);      // turn it on going forward
+    rightMotor1.run(FORWARD);     // turn it on going forward
+    rightMotor2.run(FORWARD);     // turn it on going forward
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    leftMotor1.setSpeed(speedSet);
+    leftMotor2.setSpeed(speedSet);
+    rightMotor1.setSpeed(speedSet); 
+    rightMotor2.setSpeed(speedSet);
+    delay(5);
+  }
+}
+//-------------------------------------------------------------------------------------------------------------------------------------
+void moveBackward() {
+    motorSet = "BACKWARD";
+    leftMotor1.run(BACKWARD);     // turn it on going backward
+    leftMotor2.run(BACKWARD);     // turn it on going backward
+    rightMotor1.run(BACKWARD);    // turn it on going backward
+    rightMotor2.run(BACKWARD);    // turn it on going backward
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    leftMotor1.setSpeed(speedSet);
+    leftMotor2.setSpeed(speedSet);
+    rightMotor1.setSpeed(speedSet); 
+    rightMotor2.setSpeed(speedSet); 
+    delay(5);
+  }
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+void turnRight() {
+  motorSet = "RIGHT";
+  leftMotor1.run(FORWARD);      // turn motor 1 forward
+  leftMotor2.run(FORWARD);      // turn motor 2 forward
+  rightMotor1.run(BACKWARD);    // turn motor 3 backward
+  rightMotor2.run(BACKWARD);    // turn motor 4 backward
+  rightMotor1.setSpeed(speedSet+MAX_SPEED_OFFSET);      
+  rightMotor2.setSpeed(speedSet+MAX_SPEED_OFFSET);     
+  delay(1500); // run motors this way for 1500        
+  motorSet = "FORWARD";
+  leftMotor1.run(FORWARD);      // set both motors back to forward
+  leftMotor2.run(FORWARD);
+  rightMotor1.run(FORWARD);
+  rightMotor2.run(FORWARD);      
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+void turnLeft() {
+  motorSet = "LEFT";
+  leftMotor1.run(BACKWARD);      // turn motor 1 backward
+  leftMotor2.run(BACKWARD);      // turn motor 2 backward
+  leftMotor1.setSpeed(speedSet+MAX_SPEED_OFFSET);     
+  leftMotor2.setSpeed(speedSet+MAX_SPEED_OFFSET);    
+  rightMotor1.run(FORWARD);     // turn motor 3 forward
+  rightMotor2.run(FORWARD);     // turn motor 4 forward
+  delay(1500); // run motors this way for 1500  
+  motorSet = "FORWARD";
+  leftMotor1.run(FORWARD);      // turn it on going forward
+  leftMotor2.run(FORWARD);      // turn it on going forward
+  rightMotor1.run(FORWARD);     // turn it on going forward
+  rightMotor2.run(FORWARD);     // turn it on going forward
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+void turnAround() {
+  motorSet = "RIGHT";
+  leftMotor1.run(FORWARD);      // turn motor 1 forward
+  leftMotor2.run(FORWARD);      // turn motor 2 forward
+  rightMotor1.run(BACKWARD);    // turn motor 3 backward
+  rightMotor2.run(BACKWARD);    // turn motor 4 backward
+  rightMotor1.setSpeed(speedSet+MAX_SPEED_OFFSET);      
+  rightMotor2.setSpeed(speedSet+MAX_SPEED_OFFSET);
+  delay(1700); // run motors this way for 1700
+  motorSet = "FORWARD";
+  leftMotor1.run(FORWARD);      // set both motors back to forward
+  leftMotor2.run(FORWARD);
+  rightMotor1.run(FORWARD);
+  rightMotor2.run(FORWARD);      
+}  
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+void forward_car()
+{
+leftMotor1.run(FORWARD);
+leftMotor1.setSpeed(170);
+leftMotor2.run(FORWARD);
+leftMotor2.setSpeed(170);
+rightMotor1.run(FORWARD);
+rightMotor1.setSpeed(170);
+rightMotor2.run(FORWARD);
+rightMotor2.setSpeed(170);
+delay(2000);
+moveStop();
+}
+void back_car()
+{
+leftMotor1.run(BACKWARD);
+leftMotor1.setSpeed(170);
+leftMotor2.run(BACKWARD);
+leftMotor2.setSpeed(170);
+rightMotor1.run(BACKWARD);
+rightMotor1.setSpeed(170);
+rightMotor2.run(BACKWARD);
+rightMotor2.setSpeed(170);
+delay(2000);
+moveStop();
+}
+void right_car()
+{
+/*myservo.write(0);
+ delay(1000);
+myServo.write(90);
+delay(1000);*/
+leftMotor1.run(FORWARD);
+leftMotor1.setSpeed(170);
+leftMotor2.run(FORWARD);
+leftMotor2.setSpeed(170);
+rightMotor1.run(BACKWARD);
+rightMotor1.setSpeed(170);
+rightMotor2.run(BACKWARD);
+rightMotor2.setSpeed(170);
+delay(1000);
+moveStop();
+}
+void left_car()
+{
+leftMotor1.run(BACKWARD);
+leftMotor1.setSpeed(170);
+leftMotor2.run(BACKWARD);
+leftMotor2.setSpeed(170);
+rightMotor1.run(FORWARD);
+rightMotor1.setSpeed(170);
+rightMotor2.run(FORWARD);
+rightMotor2.setSpeed(170);
+delay(1000);
+moveStop();
+}
+
