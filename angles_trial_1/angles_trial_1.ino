728e782b8cdad6e11527cbac9b89b9c6b0ea2dd0
NadaIhabAhmed <36302376+NadaIhabAhmed@users.noreply.github.com>
2018-07-06 22:16:59 +0200

    trial code no.1 for angles

---
+++ angles_trial_1/angles_trial_1.ino
@@ -0,0 +1,67 @@
+ #include <AFMotor.h> //Adafruit Motor Shield Library. First you must download and install AFMotor library
+ 
+  AF_DCMotor motor1 (1, MOTOR12_1KHZ); //create motor #1 using M1 output on Motor Drive Shield, set to 1kHz PWM frequency
+  AF_DCMotor motor2 (2, MOTOR12_1KHZ); //create motor #2 using M2 output on Motor Drive Shield, set to 1kHz PWM frequency
+  AF_DCMotor motor3 (3, MOTOR12_1KHZ); //create motor #1 using M1 output on Motor Drive Shield, set to 1kHz PWM frequency
+  AF_DCMotor motor4 (4, MOTOR12_1KHZ); //create motor #2 using M2 output on Motor Drive Shield, set to 1kHz PWM frequency
+
+float x;
+char g;
+float sp=180;
+  
+ void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600); //start serial IN CASE WE NEED TO PRINT DEBUGGING INFO
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+   
+//currentMillis=millis();  //get the current time (actually the number of milliseconds since the program started)
+if (Serial.available()>0)
+{   x=Serial.parseFloat();  //enter the degree
+    Serial.println(x);
+    
+     if (Serial.available()>0)
+     {    g= Serial.read();
+          Serial.println(g);
+      
+          if(g=='r')                 //turn right with x degrees
+         {      right_angle();   }
+   
+  
+          if (g='l')              //turn left with x degrees
+         {      left_angle();    }
+      
+     }
+}
+       
+}
+
+void right_angle()
+{
+  
+  motor1.run(FORWARD);
+  motor1.setSpeed(sp-x);
+  motor2.run(FORWARD);
+  motor2.setSpeed(sp);
+  motor3.run(FORWARD);
+  motor3.setSpeed(sp-x);
+  motor4.run(FORWARD);
+  motor4.setSpeed(sp);
+}
+
+
+void left_angle() 
+{
+  
+  motor1.run(FORWARD);
+  motor1.setSpeed(sp);
+  motor2.run(FORWARD);
+  motor2.setSpeed(sp-x);
+  motor3.run(FORWARD);
+  motor3.setSpeed(sp);
+  motor4.run(FORWARD);
+  motor4.setSpeed(sp-x);
+}
